97573cb5d89b9d3d823007901e529a93bb245b15
evertonfarias <evertonfarias1@live.com>
2024-09-16 20:20:47 -0300

    atualizado

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -7,7 +7,7 @@ export default function Header(){
     return (
         <>
             <header>
-                <h1>Minha Página React</h1>
+                <h1>React Do Datena</h1>
                 <img src={reactLogo} alt='Logo do React' />
                 <img src={viteLogo} alt='Logo do Vite' />
             </header>
